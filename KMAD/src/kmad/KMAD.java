6bdc7e8e1aecec94905cf7931062c3005773e2e4
unknown <dohertjp@DOHERTJP-1.rose-hulman.edu>
2013-09-30 17:14:02 -0400

    Basic working input scanner

--- KMAD/src/kmad/KMAD.java
+++ KMAD/src/kmad/KMAD.java
@@ -4,6 +4,11 @@
  */
 package kmad;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Ian and Joseph, hopefully.
@@ -15,5 +20,22 @@ public class KMAD {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    	File inFile = new File("TGMC training-sample.csv");
+    	Scanner in = null;
+    	ArrayList<Candidate> candidates = new ArrayList<Candidate>();
+    	
+    	try {
+    		in = new Scanner(inFile);
+		} catch (FileNotFoundException e) {
+			System.out.println("Input File not found");
+			System.exit(1); //can;t do anything, exit.
+		}
+    	
+    	String line;
+    	while(in.hasNext()){
+    		line = in.nextLine();
+    		Candidate c = new Candidate(line, true);
+    		candidates.add(c);
+    	}
     }
 }

c1250d203498df4c7ae048c375aff15e5b956020
amesen <amesen@AMESEN-1.rose-hulman.edu>
2013-10-22 19:02:53 -0400

    Work done on J48.

--- KMAD/src/kmad/J48.java
+++ KMAD/src/kmad/J48.java
@@ -12,31 +12,37 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
+/**320 columns
  *
  * @author amesen
  */
 public class J48 {
-	ArrayList<SpecTuple> output = new ArrayList<SpecTuple>();
-	ArrayList<Candidate> candidates;
-	boolean prepruning;
-	boolean postpruning;
-	File inFile;
-	int precision;
+	static ArrayList<SpecTuple> output = new ArrayList<SpecTuple>();
+	static ArrayList<Candidate> candidates;
+	static ArrayList<ArrayList<Integer>> attributes;
+	static boolean prepruning;
+	static boolean postpruning;
+	static File inFile;
+	static int precision;
+	final static File training = new File("TGMC training-sample.csv");
+	static J48DecisionTree tree;
 	
 	//Base constructer, takes a data file location, numeric precision factor, and pre/post pruning options
-	public J48(File fileLoc, int precision, boolean pre, boolean post, ArrayList<Candidate> cands){
+	public J48(File data, int precision, boolean pre, boolean post, ArrayList<Candidate> cands){
 		prepruning = pre;
 		postpruning = post;
 		candidates = cands;
-		inFile = fileLoc;
+		inFile = data;
 		this.precision = precision;
 		
 	}
 	
 	public static void process(){
 		//read in first question set, which will be training (not actual file format, check)
-		readIn();
+		readIn(true);
+		
+		//Preprocess the training data so that the tree can be built
+		preProcess();
 		
 		//Build a decision tree based on the given training data
 		buildDecisionTree();
@@ -44,31 +50,38 @@ public class J48 {
 		//Loop for each question, reading in only the candidates for the
 		//current question.
 		for(int i=0; i<8; i++){
-			readIn();
-			preProcess();
+			readIn(false);
 			processSingleQuestion();
 		}
 	}
 	
 	//Reads candidates for 1 question
-	public static void readIn(){
+	public static void readIn(boolean isTraining){
+		//cands will be built here if not passed in, still being discussed with group
+		//Ask Ian for help with IO
+		
+		if(isTraining){
+			//read in File training
+		}else{
+			//read in File inFile
+		}
 		
 	}
 	
 	//Builds a new decision tree based on the given training set
 	public static void buildDecisionTree(){
-		J48DecisionTree tree = new J48DecisionTree();
+		tree = new J48DecisionTree(attributes, precision);
 	}
 	
 	//Takes the training dataset read in by readIn and switches the columns and rows into
 	//a new ArrayList containing each column as an entry. ONLY NEEDED FOR TRAINING DATA.
 	public static void preProcess(){
-		//TODO: fill in
+		//Ask Ian what the hell we're doing with Candidates
 	}
 	
 	//Runs a single question through the tree, using actual data (not training)
 	public static void processSingleQuestion(){
-		//TODO
+		tree.analyze(candidates);
 	}
 	
 }
--- KMAD/src/kmad/J48DecisionTree.java
+++ KMAD/src/kmad/J48DecisionTree.java
@@ -11,8 +11,73 @@ import java.util.ArrayList;
  */
 public class J48DecisionTree {
 	//TODO: Fill in class ASAP
+	Node root = new Node(null);
+	ArrayList<ArrayList<Integer>> masterSet;
+	int precision;
 	
-	public J48DecisionTree(){
+	public J48DecisionTree(ArrayList<ArrayList<Integer>> passedIn, int precision){
+		masterSet = passedIn;
+		this.precision = precision;
+		
+		//Take the given attributes and pick the cut value based on the precision value
+		precisionMeasure();
+		
+		//Divide up the sets according to the afore-chosen cuts
+		precisionCut();
+		
+		//Use the now-divided sets to build the decision tree
+		buildTree();
+		
+		//Done! Ready for candidates.
+		
+	}
+	
+	/**Sets the cutoff points for each attribute set and adds them to then end
+	 * of the ArrayList masterSet
+	**/
+	private void precisionMeasure(){
+		int max=0, min=999999999;
+		
+		//loop through to find max and min
+		for(int i=0; i<masterSet.size(); i++){
+			for(int j=0; j<masterSet.get(i).size(); j++){
+				if(masterSet.get(i).get(j) > max){
+					max = masterSet.get(i).get(j);
+				}if(masterSet.get(i).get(j) < min){
+					min = masterSet.get(i).get(j);
+				}
+			}
+			//take max and min for the current attribute set and add the cuts to the end of the arraylist
+			for(int l=0; l<precision; l++){
+				masterSet.get(i).add( (((max - min)/(precision+1)) * (l+1)) + min );
+			}
+		}
+	}
+	
+	private void precisionCut(){
+		int size = masterSet.get(0).size() - precision;
+		
+		for(int i=0; i<masterSet.size(); i++){
+			for(int k=0; k<size; k++){
+				for(int l=0; l<precision; l++){
+					if(masterSet.get(i).get(k) < masterSet.get(i).get(size+1)){
+						
+					}
+				}
+			}
+		}
+		
+	}
+	
+	public SpecTuple analyze( ArrayList<Candidate> question ){
+		//TODO: Fill in
+		
+		
+		return new SpecTuple(1,2);
+	}
+	
+	//Used by the constructor to build a decision tree using the preprocessed training dataset
+	private void buildTree(){
 		
 	}
 	
@@ -21,31 +86,41 @@ public class J48DecisionTree {
 class Node {
 	private Node child[];
 	private Node parent;
-	private ArrayList<Float> T = new ArrayList();
+	public ArrayList<Float> T = new ArrayList();
 	private boolean type;
+	private boolean isRoot;
+	private boolean isLeaf = false;
 	
+	//Constructor. If the node is to be the root, pass in null as the parent type
 	public Node(Node parent){
+		if(parent == null){
+			isRoot = true;
+		}else{
+			isRoot = false;
+		}
 		this.parent = parent;
 	}
 	
 	public boolean isRoot(){
-		if(parent == null){
-			return true;
-		}else{
-			return false;
-		}
+		return isRoot;
 	}
+
 	
 	public boolean isLeaf(){
-		if(child.length == 0){
-			return true;
-		}else{
-			return false;
-		}
+		return isLeaf;
 	}
 	
 	public void setLeaf(boolean cl){
 		type = cl;
+		isLeaf = true;
+	}
+	
+	public boolean hasChild(){
+		if(child.length > 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 	
